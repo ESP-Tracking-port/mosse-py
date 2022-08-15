93c4d1df87a71c767dab6af4e3ee3d7ae5b27a58
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-15 17:56:04 +0300

    [:mosse] Enh | PSR threshold

--- mosse.py
+++ mosse.py
@@ -2,7 +2,12 @@ import numpy as np
 import cv2
 import random
 import sys
-import matplotlib.pyplot as plt
+
+PSR_THRESHOLD = 2
+
+
+def print_override(*args, **kwargs):
+    pass
 
 
 class MOSSE:
@@ -17,9 +22,9 @@ class MOSSE:
     def __init__(self, frame, roi, learning_rate, train_num, sigma):
         self.frame = frame
         self.roi = roi                      # contains (top_left_x, top_left_y, width, height)
-        self.learning_rate = learning_rate  # running average parameter 
+        self.learning_rate = learning_rate  # running average parameter
         self.train_num = train_num          # number of training examples to be trained on
-        self.sigma = sigma                  # parameter for calculating gaussian map 
+        self.sigma = sigma                  # parameter for calculating gaussian map
 
 
     def get_gaussian_map(self):
@@ -30,16 +35,16 @@ class MOSSE:
         x, y, w, h = self.roi
         center_x = x + w/2
         center_y = y + h/2
-        
+
         # create a rectangular grid out of two given one-dimensional arrays
         xx, yy = np.meshgrid(np.arange(x, x+w), np.arange(y, y+h))
 
         # calculating distance of each pixel from roi center
         dist = (np.square(xx - center_x) + np.square(yy - center_y)) / (2*self.sigma)
-        
+
         response = np.exp(-dist)
         response = (response - response.min()) / (response.max() - response.min())
-        
+
         return response
 
 
@@ -59,20 +64,20 @@ class MOSSE:
         '''
 
         h, w = f.shape
-        
+
         # transformation using a log function which helps with low contrast lighting situations.
         img = np.log(f+1)
-        
+
         # pixel values are normalized to have a mean value of 0.0 and a norm of 1.0.
         img = (img - np.mean(img)) / (np.std(img))
-        
+
         # image is multiplied by a cosine window which gradually reduces the pixel values near the edge to zero.
         window_col = np.hanning(w)  # returns a hanning window....Hanning window is a taper formed by using a weighted cosine
         window_row = np.hanning(h)
         col_mask, row_mask = np.meshgrid(window_col, window_row)
         window = col_mask * row_mask
         img = img * window
-        
+
         return f
 
 
@@ -92,9 +97,9 @@ class MOSSE:
         rot_img = cv2.warpAffine(self.frame, rotate_mat, (self.frame.shape[1], self.frame.shape[0]))
 
         x, y, w, h = self.roi
-        
+
         return rot_img[y:y+h, x:x+w]
-        
+
 
     def pre_training(self):
         '''
@@ -106,27 +111,27 @@ class MOSSE:
         '''
 
         x, y, w, h = self.roi
-        
+
         self.get_G()    # initializes centered 2D gaussian map
-        
+
         f = self.preprocessing(self.frame[y:y+h, x:x+w])
         F = np.fft.fft2(f)
-        
+
         self.Ai = self.G * np.conjugate(F)
         self.Bi = F * np.conjugate(F)
-        
+
         for i in range(self.train_num):
             f = self.preprocessing(self.get_rand_affine())
             F = np.fft.fft2(f)
-            
+
             self.Ai += self.G * np.conjugate(F)
             self.Bi += F * np.conjugate(F)
 
 
     def update_frame(self, frame):
         self.frame = frame
-    
-    
+
+
     def get_new_roi(self):
         '''
         Get new position of roi by applying filter
@@ -134,36 +139,36 @@ class MOSSE:
         '''
 
         x, y, w, h = self.roi
-        
+
         self.Hi = self.Ai / self.Bi
-        
+
         f = self.preprocessing(self.frame[y:y+h, x:x+w])
         F = np.fft.fft2(f)
-        
+
         self.G = self.Hi * F
         g = np.fft.ifft2(self.G)
-        
+
         # mapping gaussian map to 0-1
         g = (g - g.min()) / (g.max() - g.min())
-        print('g',g)
+        print_override('g',g)
         max_value = np.max(g)
         max_pos = np.where(g == max_value)
         dy = int(np.mean(max_pos[0]) - g.shape[0] / 2)
         dx = int(np.mean(max_pos[1]) - g.shape[1] / 2)
 
         psr = self.psr(g)
-        if psr <= 6:
+        if psr <= PSR_THRESHOLD:
             dx=0
             dy=0
             print('OBJECT OCCLUDED!!')
-        print('psr:',psr)
+        print_override('psr:',psr)
         return (x+dx, y+dy, w, h) , psr
 
 
     def update_roi(self, roi):
         self.roi = roi
 
-    
+
     def update(self):
         '''
         Update the filter using current frame features
@@ -173,16 +178,16 @@ class MOSSE:
         '''
 
         x, y, w, h = self.roi
-        
+
         self.get_G()    # initializes centered 2D gaussian map
 
         f = self.preprocessing(self.frame[y:y+h, x:x+w])
         F = np.fft.fft2(f)
-        
+
         # filters need to quickly adapt in order to follow objects. Running average is used for this purpose.
         self.Ai = self.learning_rate * self.G * np.conjugate(F) + (1 - self.learning_rate) * self.Ai
         self.Bi = self.learning_rate * F * np.conjugate(F) + (1 - self.learning_rate) * self.Bi
-    
+
     def psr(self,g):
         g_max = np.max(g)
         x, y, w, h = self.roi
