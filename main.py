b1dddb638add5a99f753f7e2e74e597075e142e7
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-15 18:04:16 +0300

    [:main] Enh | `waitKey` timeout

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ while(True):
     cv2.rectangle(frame, (x, y),(x + w, y + h), (0,255,0), 2)
     cv2.imshow('frame',frame)
 
-    k = cv2.waitKey(1) & 0xff
+    k = cv2.waitKey(30) & 0xff
     if k == 27 :
         break
 
